3477b6b906a90225ced20ffc570d3c20f400033d
QuentinGra <quentin.grange@orange.fr>
2024-03-27 16:59:31 +0100

    modifies flash msg

--- app/Views/Layout/flash.php
+++ app/Views/Layout/flash.php
@@ -1,5 +1,5 @@
 <?php foreach (isset($_SESSION['messages']) ? $_SESSION['messages'] : [] as $type => $message) : ?>
-    <div class="alert alert<?= $type; ?>">
+    <div class="alert alert-<?= $type; ?> text-center">
         <?php
         echo $message;
         unset($_SESSION['messages'][$type]);
