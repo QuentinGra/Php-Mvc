7e7d920dc862d39505de1fa7b696ab0abc76a6d9
QuentinGra <quentin.grange@orange.fr>
2024-03-27 12:25:48 +0100

    add bootstrap

--- app/Views/Frontend/Articles/show.php
+++ app/Views/Frontend/Articles/show.php
@@ -1,2 +1,8 @@
-<h1>Page de détail d'un article</h1>
-<?php var_dump($article) ?>
+<section class="container mt-2">
+    <h1 class="text-center"><?= $article->getTitle(); ?></h1>
+    <div class="card">
+        <div class="card-body">
+            <p class="card-text"><?= $article->getContent(); ?></p>
+        </div>
+    </div>
+</section>
