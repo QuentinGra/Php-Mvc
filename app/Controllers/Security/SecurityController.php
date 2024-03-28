a75aad3389a08dac97ca24e9120773ea6b6dafdb
QuentinGra <quentin.grange@orange.fr>
2024-03-28 11:46:39 +0100

    add register

--- app/Controllers/Security/SecurityController.php
+++ app/Controllers/Security/SecurityController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controllers\Security;
 
-use App\Core\BaseController;
 use App\Core\Route;
-use App\Form\LoginForm;
 use App\Models\User;
+use App\Form\LoginForm;
+use App\Form\RegisterForm;
+use App\Core\BaseController;
 
 class SecurityController extends BaseController
 {
@@ -44,5 +45,45 @@ class SecurityController extends BaseController
         }
 
         $this->redirect('/');
+    }
+
+    #[Route('/register', 'app.register', ['GET', 'POST'])]
+    public function register(): void
+    {
+        $form = new RegisterForm('/register');
+
+        if ($form->validate($_POST, ['firstName', 'lastName', 'email', 'password'])) {
+            $firstName = trim(strip_tags($_POST['firstName']));
+            $lastName = trim(strip_tags($_POST['lastName']));
+            $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+            $password = password_hash($_POST['password'], PASSWORD_ARGON2I);
+
+            if ($email) {
+                if (!(new User)->findByEmail($email)) {
+                    (new User)
+                        ->setFirstName($firstName)
+                        ->setLastName($lastName)
+                        ->setEmail($email)
+                        ->setPassword($password)
+                        ->create();
+
+                    $_SESSION['messages']['success'] = "Vous êtes bien inscrit à l'application";
+                    $this->redirect('/');
+                } else {
+                    $_SESSION['messages']['danger'] = "L'email est déja utilisé par un autre compte";
+                }
+            } else {
+                $_SESSION['messages']['danger'] = "Veuillez renseigner un email valide";
+            }
+        } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $_SESSION['messages']['danger'] = "Veuillez remplir tous les champs obligatoires";
+        }
+
+        $this->render('Security/register.php', [
+            'form' => $form->createView(),
+            'meta' => [
+                'title' => 'Inscription',
+            ]
+        ]);
     }
 }
---
+++ app/Views/Security/register.php
@@ -0,0 +1,4 @@
+<section class="container mt-2">
+    <h1 class="text-center">S'inxcrire</h1>
+    <?= $form; ?>
+</section>
